b6ec719ccf82ab8426a71a4194101cc1a924babb
Alexa Amoriello (RIT Student) <axa9517@rit.edu>
2023-06-06 09:14:47 -0400

    Player movement with new input system
    Movement with touch. Turn on simulating touch with mouse click in window setting to test.

--- IGME-Microgames/Assets/Scripts/Player/Movement.cs
+++ IGME-Microgames/Assets/Scripts/Player/Movement.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Player object
+    public GameObject player;
+
+    // Player input system 
+    private PlayerInput playerInput;
+    private InputAction touchMovementAction;
+    private InputAction touchPressAction;
+
+    // World position of the mouse press
+    private Vector3 worldpos;
+
+    /// <summary>
+    /// Awake function that pulls the default player movement
+    /// </summary>
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        touchPressAction = playerInput.actions.FindAction("TouchPress");
+        touchMovementAction = playerInput.actions.FindAction("TouchMovement");
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     void Update()
     {
-        
+    }
+
+    /// <summary>
+    /// Subscribe after press
+    /// </summary>
+    private void OnEnable()
+    {
+        touchPressAction.performed += TouchPressed;
+    }
+
+    /// <summary>
+    /// Unsubscribe after press
+    /// </summary>
+
+    private void OnDisable()
+    {
+        touchPressAction.performed -= TouchPressed;
+    }
+
+    /// <summary>
+    /// On Press, find the press position and convert it to the world position for player movement.
+    /// </summary>
+    /// <param name="context"></param>
+    private void TouchPressed(InputAction.CallbackContext context)
+    {
+        Vector2 screenPos = touchMovementAction.ReadValue<Vector2>();
+        worldpos = Camera.main.ScreenToWorldPoint(screenPos);
+
+        worldpos.z = player.transform.position.z;
+        player.transform.position = worldpos;
+    }
+
+    public Vector3 GetWorldPosToPoint()
+    {
+        return worldpos;
     }
 }
