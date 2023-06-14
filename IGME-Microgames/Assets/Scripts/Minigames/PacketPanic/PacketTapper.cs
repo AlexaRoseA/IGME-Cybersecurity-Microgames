d79e328d6682037fb405558131a22b2f02a7fd0c
Henry Orsagh <h.orsagh@gmail.com>
2023-06-14 09:24:57 -0400

    Merge branch 'dev' into packet-panic

--- IGME-Microgames/Assets/Scripts/Minigames/PacketPanic/PacketTapper.cs
+++ IGME-Microgames/Assets/Scripts/Minigames/PacketPanic/PacketTapper.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PacketTapper : MonoBehaviour
+public class PacketTapper : InputHandler
 {
-    // Start is called before the first frame update
-    void Start()
+
+    protected override void TouchPressed(InputAction.CallbackContext context)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void TouchCancelled(InputAction.CallbackContext context)
     {
-        
+
     }
 }
--- IGME-Microgames/Assets/Scripts/Player/InputHandler.cs
+++ IGME-Microgames/Assets/Scripts/Player/InputHandler.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private PlayerInput playerInput;
+    private InputAction touchMovementAction;
+    private InputAction touchPressAction;
+
+    protected void Awake()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
+        touchPressAction = playerInput.actions.FindAction("TouchPress");
+        touchMovementAction = playerInput.actions.FindAction("TouchMovement");
     }
 
-    // Update is called once per frame
-    void Update()
+    protected void OnEnable()
     {
-        
+        touchPressAction.performed += TouchPressed;
+        touchPressAction.canceled += TouchCancelled;
+    }
+
+    /// <summary>
+    /// Unsubscribe after press
+    /// </summary>
+
+    protected void OnDisable()
+    {
+        touchPressAction.performed -= TouchPressed;
+        touchPressAction.canceled -= TouchCancelled;
+    }
+
+    /// <summary>
+    /// On Press, find the press position and convert it to the world position for player movement.
+    /// </summary>
+    /// <param name="context"></param>
+    protected virtual void TouchPressed(InputAction.CallbackContext context)
+    {
+    }
+
+    /// <summary>
+    /// On Press, find the press position and convert it to the world position for player movement.
+    /// </summary>
+    /// <param name="context"></param>
+    protected virtual void TouchCancelled(InputAction.CallbackContext context)
+    {
+    }
+
+    protected Vector3 TouchScreenToWorld()
+    {
+        Vector3 screenPos = touchMovementAction.ReadValue<Vector2>();
+        Vector3 worldpos = Camera.main.ScreenToWorldPoint(screenPos);
+        worldpos.z = player.transform.position.z;
+        return worldpos;
     }
 }
