2238b3b1642879c8a983c0378870d54d0640b1e5
Henry Orsagh <h.orsagh@gmail.com>
2023-06-27 17:29:02 -0400

    fleeing should end in corners less

--- IGME-Microgames/Assets/Scripts/Minigames/PacketPanic/Phase2/PathwayFleer.cs
+++ IGME-Microgames/Assets/Scripts/Minigames/PacketPanic/Phase2/PathwayFleer.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml;
 using Unity.Collections;
 using Unity.PlasticSCM.Editor.WebApi;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem.XR;
 using UnityEngine.Tilemaps;
@@ -17,50 +19,34 @@ public class PathwayFleer : PathwayNavigation
 
     private Vector3 fleeDirection;
 
-    private List<Vector3> neighborCells;
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = new Color(1f, 0f, 0f, .5f);
-        //Gizmos.DrawRay(fleeDirection * -10 + transform.position, fleeDirection * 10);
-        Gizmos.color = new Color(0f, 1f, 0f, .5f);
-        if(neighborCells != null)
-        {
-            foreach (Vector3 cell in neighborCells)
-            {
-                Gizmos.DrawSphere(cell, 0.6f);
-            }
-
-        }
-    }
-
     protected override Node DetermineDestinationNode(List<Node> failed)
-    {
-        //raycast from past fleer to fleer
-
+    { 
         //get furthest tile in direct line away from chaser
         fleeDirection = Vector3.Normalize(fleeTarget.transform.position - transform.position);
 
         RaycastHit2D fleeDestinationCheck = Physics2D.Raycast(fleeDirection * -10 + transform.position, fleeDirection * 10);
-
-
-        //Debug.Log(fleeDestinationCheck.collider.gameObject.GetComponent<Node>().cellPosition);
-
-        //distance to furthest tile is low?
-        //try at an angle, until finding one 
-
         Node furthestLinearDestination = fleeDestinationCheck.collider.gameObject.GetComponent<Node>();
 
 
-        List<Node> neighborNodes = tilemap.GetTile<NodeTiles>(Vector3Int.zero).FindNeighbors(tilemap.WorldToCell(transform.position));
-        neighborCells = new List<Vector3>();
-        foreach(Node neighbor in neighborNodes)
-        {
-            neighborCells.Add(tilemap.CellToWorld(neighbor.cellPosition));
-        }
 
-        if(!failed.Contains(furthestLinearDestination)) 
-            return furthestLinearDestination;
+        NodeTiles nodeTile = tilemap.GetTile<NodeTiles>(Vector3Int.zero);
+        //if furthestLinear isn't this node, and isn't one of this node's neighbors
+        while(furthestLinearDestination.cellPosition != tilemap.WorldToCell(transform.position) &&
+            !nodeTile.FindNeighbors(tilemap.WorldToCell(transform.position)).Contains(furthestLinearDestination))
+        {
+            //if it hasn't failed, return furthest
+            if(!failed.Contains(furthestLinearDestination))
+            {
+                return furthestLinearDestination;
+            }
+
+            //if it has failed, try closer
+            RaycastHit2D[] hits = new RaycastHit2D[0];
+            furthestLinearDestination.GetComponent<CircleCollider2D>().Raycast(fleeDirection * 10, hits);
+            furthestLinearDestination = hits[0].collider.gameObject.GetComponent<Node>();
+        }
+        
+        //otherwise, try at an angle
 
         return null;
     }
