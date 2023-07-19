d0c323d79ff652c4745a0b4c8ed28671c29e6751
Alexa Amoriello <axa9517@rit.edu>
2023-07-19 14:13:27 -0400

    Base scripts for scan
    Starts scan (pauses buttons), determine scan results, and returns. Need to add orb repaint

--- IGME-Microgames/Assets/Scripts/Minigames/FireDefense/Tutorial/FireDefense_T_ScanGame.cs
+++ IGME-Microgames/Assets/Scripts/Minigames/FireDefense/Tutorial/FireDefense_T_ScanGame.cs
@@ -6,21 +6,22 @@ using Yarn.Unity;
 
 public class FireDefense_T_ScanGame : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Bot storage information - current and prior
     private GameObject currentBot;
     private Animator currentBotAnimator;
     private GameObject priorBot;
+
+    // All potential bot choices + scanbar
     [SerializeField] List<GameObject> botChoices;
     [SerializeField] GameObject scanBar;
-    private bool scanned = false;
 
+    // Input fields and spawn pos
     [SerializeField] Button scan;
     [SerializeField] Button approve;
     [SerializeField] Button deny;
-
     [SerializeField] Transform standPos;
-    private FireDefenseT_CameraPan cameraScript;
 
+    // Timer and game start variables
     private float seconds = 1.5f;
     private float timer;
     private float percent;
@@ -30,10 +31,11 @@ public class FireDefense_T_ScanGame : MonoBehaviour
     public InMemoryVariableStorage variableStorage;
     public DialogueRunner dialogueRunner;
 
-
+    /// <summary>
+    /// Grabs all necessary variabes
+    /// </summary>
     void Start()
     {
-        cameraScript = FindObjectOfType<FireDefenseT_CameraPan>();
         ResetScans();
         scan.interactable = false;
         priorBot = null;
@@ -41,7 +43,9 @@ public class FireDefense_T_ScanGame : MonoBehaviour
         dialogueRunner.StartDialogue("FireDefense_T_2");
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Spawns and resets scans based on current bot being null
+    /// </summary>
     void Update()
     {
         if(startGame && currentBot == null)
@@ -51,6 +55,10 @@ public class FireDefense_T_ScanGame : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Spawns a bot based on either random choice or force choice
+    /// </summary>
+    /// <param name="forceChoice"></param>
     [YarnCommand("SpawnBot")]
     public void SpawnBot(string forceChoice = "")
     {
@@ -70,6 +78,8 @@ public class FireDefense_T_ScanGame : MonoBehaviour
                 if(botChoice.name == forceChoice)
                 {
                     currentBot = Instantiate(botChoice, standPos.position, Quaternion.identity);
+                    currentBotAnimator = currentBot.transform.GetChild(0).GetComponent<Animator>();
+                    break;
                 }
             }
         }
@@ -77,6 +87,9 @@ public class FireDefense_T_ScanGame : MonoBehaviour
         currentBotAnimator = currentBot.transform.GetChild(0).GetComponent<Animator>();
     }
 
+    /// <summary>
+    /// Starts the game loop
+    /// </summary>
     [YarnCommand("StartGame")]
     public void StartGame()
     {
@@ -90,6 +103,9 @@ public class FireDefense_T_ScanGame : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Destroys all bits on screen
+    /// </summary>
     [YarnCommand("DestroyAllBits")]
     public void DestroyAllBits()
     {
@@ -100,12 +116,21 @@ public class FireDefense_T_ScanGame : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Pass go yarn command
+    /// </summary>
+    /// <param name="force"></param>
     [YarnCommand("PassGo")]
     public void YouPassGo(string force = "")
     {
         StartCoroutine(BitGo(force));
     }
 
+    /// <summary>
+    /// Has the bit progress, makes them walk off screen
+    /// </summary>
+    /// <param name="force">force spawn bot as next</param>
+    /// <returns></returns>
     IEnumerator BitGo(string force = "")
     {
         float timer = 0;
@@ -127,34 +152,80 @@ public class FireDefense_T_ScanGame : MonoBehaviour
         yield return null;
     }
 
+    /// <summary>
+    /// Runs animator, plays death animation
+    /// </summary>
+    /// <param name="force">force spawn bot as next</param>
+    /// <returns></returns>
     IEnumerator BitDead(string force = "")
     {
         yield return null;
     }
 
-    public void AcceptButton()
+    /// <summary>
+    /// Completes a set task based on good/bad.
+    /// </summary>
+    /// <param name="value"></param>
+    public void CheckStatus(string value)
     {
+        value = value.ToLower();
         string type = currentBot.name.Split("_")[1];
         type = type.Substring(0, type.Length - 8).ToLower();
 
         Debug.Log(type);
-        StartCoroutine(BitGo());
+
+        switch(type)
+        {
+            case "good":
+                if(value == type)
+                {
+                    //give positive stuff - good passed
+                    StartCoroutine(BitGo());
+                } else
+                {
+                    //give negative stuff - bad passed
+                    StartCoroutine(BitGo());
+                }
+                break;
+            case "bad":
+                if (value == type)
+                {
+                    //give positive stuff - bad dead
+                    StartCoroutine(BitDead());
+                }
+                else
+                {
+                    //give negative stuff - good dead
+                    StartCoroutine(BitDead());
+                }
+                break;
+        }
+
         ResetScans();
     }
 
+    /// <summary>
+    /// Resets the screen for a new scanner
+    /// </summary>
     private void ResetScans()
     {
-        scanned = false;
         approve.interactable = false;
         deny.interactable = false;
         scan.interactable = true;
     }
 
+    /// <summary>
+    /// Starts the scan
+    /// </summary>
     public void StartScan()
     {
         StartCoroutine(Scan());
     }
 
+    /// <summary>
+    /// Scan animation
+    /// </summary>
+    /// <returns></returns>
     IEnumerator Scan()
     {
         scan.interactable = false;
@@ -193,7 +264,6 @@ public class FireDefense_T_ScanGame : MonoBehaviour
         }
 
         scanBar.SetActive(false);
-        scanned = true;
         approve.interactable = true;
         deny.interactable = true;
         timer = 0;
