8749722058c6f966c8bd2f0b6832c2a64cd8e61f
Alexa <42698056+AlexaRoseA@users.noreply.github.com>
2023-10-16 12:20:54 -0400

    Color customization on player profile
    color customization for everything: eyes have not been touched need to change some things about how it works (not using 1 sprite for both: switch to 2 so there can be 2 different eye colors)

--- IGME-Microgames/Assets/Scripts/UIUX/SetColorFromMaterial.cs
+++ IGME-Microgames/Assets/Scripts/UIUX/SetColorFromMaterial.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetColorFromMaterial : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    public Material copyMat;
     void Start()
     {
-        
+        GetComponent<Image>().color = copyMat.color;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
